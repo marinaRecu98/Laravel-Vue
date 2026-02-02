f8abcaccd2d036016ab4cc13764832d218351fe2
marina <marina.recuenco@gmail.com>
2026-02-02 11:46:16 +0100

    primer commit

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import Header from "@/Layouts/Header.vue";
+  import Footer from "@/Layouts/Footer.vue";
+  import Nav from "@/Layouts/Nav.vue";
+  import Main from "@/Pages/Main.vue";
+</script>
+
+<template>
+  <Header />
+  <Nav />
+    <slot />
+  <Footer />
+
+</template>
+
+<style scoped>
+
+</style>
